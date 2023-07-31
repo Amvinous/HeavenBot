ffdc5a1f9cc0013a4a5517f8ab4c9d92b69e5276
Ambi <necrogamersltu@gmail.com>
2023-07-31 18:21:35 +0200

    Made bot

--- bot.py
+++ bot.py
@@ -21,6 +21,7 @@ class MyClient(discord.Client):
         self.tree = app_commands.CommandTree(self)
 
     async def setup_hook(self):
+        self.tree.copy_global_to(guild=MY_GUILD)
         await self.tree.sync(guild=MY_GUILD)
 
 
